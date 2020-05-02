4ef1adb287f9d3cbbbc9fa011c9a5abcbd7c72f6
Steven Chan <erueat@126.com>
2020-05-02 17:44:58 +0800

    fix orderBy default action as ascending; add take, skip, first, last methods; add descending orderBy;

--- cpplinq/include/cpplinq.h
+++ cpplinq/include/cpplinq.h
@@ -5,9 +5,111 @@
 namespace zen
 {
 template <typename T>
+using DefaultCondition = bool(*)(const T&);
+
+enum Order
+{
+    Ascend = 0,
+    Descend = 1
+};
+
+template <typename T, typename Condition = DefaultCondition<T>>
 class CppLinq
 {
+    class iterator
+    {
+    public:
+        iterator(std::vector<T>& o, typename std::vector<T>::const_iterator it, Condition cond) : list(o), iter(it), condition(cond)
+        {
+            while (iter != list.end() && !condition(*iter))
+            {
+                iter++;
+            }
+        }
+
+        iterator& operator++()
+        {
+            do
+            {
+                iter++;
+            } while (iter != list.end() && !condition(*iter));
+            return *this;
+        }
+
+        iterator& operator++(int)
+        {
+            iterator it(*this);
+            do
+            {
+                iter++;
+            } while (iter != list.end() && !condition(*iter));
+            return it;
+        }
+
+        iterator operator+(size_t steps)
+        {
+            iterator it = *this;
+            while(steps--)
+            {
+                ++it;
+            }
+            return it;
+        }
+
+        iterator operator-(size_t steps)
+        {
+            iterator it = *this;
+            while (steps != 0 && it.iter != list.begin())
+            {
+                if (condition(*iter))
+                {
+                    steps--;
+                }
+                it.iter--;
+            }
+            return it;
+        }
+
+        bool operator==(const iterator& r)
+        {
+            return iter == r.iter;
+        }
+
+        bool operator!=(const iterator& r)
+        {
+            return iter != r.iter;
+        }
+
+        const T& operator*()
+        {
+            return *iter;
+        }
+
+    private:
+        std::vector<T>& list;
+        typename std::vector<T>::const_iterator iter;
+        Condition condition;
+    };
 public:
+    CppLinq()
+    {
+        m_condition = [](const T&) { return true; };
+    }
+
+    CppLinq(Condition& condition, std::vector<T>& v) : m_array(std::move(v)), m_condition(condition)
+    {
+    }
+
+    CppLinq(const CppLinq& r) : m_condition(r.m_condition)
+    {
+        m_array = r.m_array;
+    }
+
+    CppLinq(CppLinq&& r) : m_condition(r.m_condition)
+    {
+        m_array = std::move(r.m_array);
+    }
+
     template <typename Array>
     CppLinq& from(Array& array)
     {
@@ -16,25 +118,39 @@ public:
         return *this;
     }
 
-    template <typename Condition>
-    CppLinq& where(Condition condition)
+    CppLinq& take(size_t count)
     {
-        std::vector<T> array;
-        for (T element : m_array)
-        {
-            if (condition(element))
-            {
-                array.push_back(element);
-            }
-        }
-        m_array = array;
+        m_takeCount = count;
         return *this;
     }
 
-    template <typename GetOrderKey>    
-    CppLinq& orderBy(GetOrderKey getOrderKey)
+    CppLinq& skip(size_t count)
     {
-        auto sortFunc = [&](const T& l, const T& r){ return (getOrderKey(l) > getOrderKey(r)); };
+        m_skipCount = count;
+        return *this;
+    }
+
+    const T& first()
+    {
+        return *begin();
+    }
+
+    const T& last()
+    {
+        return *(end() - 1);
+    }
+
+    template <typename Condition2>
+    CppLinq<T, Condition2> where(Condition2 condition)
+    {
+        CppLinq<T, Condition2> linq(condition, m_array); 
+        return linq;
+    }
+
+    template <typename GetOrderKey>    
+    CppLinq& orderBy(GetOrderKey getOrderKey, Order order = Ascend)
+    {
+        auto sortFunc = [&](const T& l, const T& r){ return order == Ascend ? (getOrderKey(l) < getOrderKey(r)) : (getOrderKey(l) > getOrderKey(r)); };
         std::sort(m_array.begin(), m_array.end(), sortFunc);
         return *this;
     }
@@ -43,15 +159,32 @@ public:
     std::vector<std::tuple<Types...>> select(SelectFunc selectFunc)
     {
         std::vector<std::tuple<Types...>> result;
-        for (T& element : m_array)
+        size_t count = m_takeCount;
+        for (const T& ele : *this)
         {
-            result.push_back(selectFunc(element));
+            if (count--)
+                result.push_back(selectFunc(ele));
+            else
+                break;
         }
         return result;
     }
 
+protected:
+    iterator begin()
+    {
+        return iterator(m_array, m_array.begin(), m_condition) + m_skipCount;
+    }
+
+    iterator end()
+    {
+        return iterator(m_array, m_array.end(), m_condition);
+    }
 private:
     std::vector<T> m_array;
+    size_t m_skipCount = 0;
+    size_t m_takeCount = SIZE_T_MAX;
+    Condition m_condition;
 };
 };
 
@@ -60,7 +193,12 @@ private:
 #define CPPLINQ(Type) zen::CppLinq<Type>()
 #define FROM(o) .from(o)
 #define WHERE(condition) .where([](const auto& o) { return condition; })
-#define ORDERBY(key) .orderBy([](const auto& o) { return key; })
+#define ORDERBY(key, Args...) .orderBy([](const auto& o) { return key; }, ##Args)
+#define DESCEND zen::Descend
+#define TAKE(count) .take(count)
+#define SKIP(count) .skip(count)
+#define FIRST .first()
+#define LAST .last()
 #define SELECT(...) .select<__VA_ARGS__>
 #define ON(...) ([](const auto& o) { return std::make_tuple(__VA_ARGS__); })
 
--- cpplinq/test/unittests/linq_unittest.cpp
+++ cpplinq/test/unittests/linq_unittest.cpp
@@ -16,9 +16,76 @@ TEST(CppLinq, basic)
     auto result = CPPLINQ(Table)
         FROM (numbers)
         WHERE (o.x % 2 == 0)
-        ORDERBY (o.x)
         SELECT (int, int) ON (o.x, o.y * o.y);
 
-    std::vector<std::tuple<int,int>> expectedResult = { { 4, 169 }, { 2, 144 } };
+    std::vector<std::tuple<int,int>> expectedResult = { { 2, 144 }, { 4, 169 } };
     EXPECT_EQ(result, expectedResult); 
+}
+
+TEST(CppLinq, take)
+{
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    auto result = CPPLINQ(int)
+        FROM (numbers)
+        WHERE (o % 2 == 0)
+        TAKE (3)
+        SELECT (int) ON (o);
+
+    std::vector<std::tuple<int>> expectedResult = { { 2 }, { 4 }, { 6 } };
+    EXPECT_EQ(result, expectedResult);
+}
+
+TEST(CppLinq, skip)
+{
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    auto result = CPPLINQ(int)
+        FROM (numbers)
+        WHERE (o % 2 == 0)
+        SKIP (3)
+        SELECT (int) ON (o);
+
+    std::vector<std::tuple<int>> expectedResult = { { 8 }, { 10 } };
+    EXPECT_EQ(result, expectedResult);
+}
+
+TEST(CppLinq, orderBy)
+{
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+    auto result = CPPLINQ(int)
+        FROM (numbers)
+        WHERE (o % 2 == 0)
+        ORDERBY (o)
+        SELECT (int) ON (o);
+
+    std::vector<std::tuple<int>> expectedResult = { { 2 }, { 4 }, { 6 }, { 8 }, { 10 } };
+    EXPECT_EQ(result, expectedResult);
+
+    auto result2 = CPPLINQ(int)
+        FROM (numbers)
+        WHERE (o % 2 == 0)
+        ORDERBY (o, DESCEND)
+        SELECT (int) ON (o);
+
+    std::vector<std::tuple<int>> expectedResult2 = { { 10 }, { 8 }, { 6 }, { 4 }, { 2 } };
+    EXPECT_EQ(result2, expectedResult2);
+}
+
+TEST(CppLinq, firstAndLast)
+{
+    std::vector<int> numbers = { 1, 2, 3 };
+
+    auto result = CPPLINQ(int)
+        FROM (numbers)
+        FIRST;
+    
+    EXPECT_EQ(result, 1);
+
+    auto result2 = CPPLINQ(int)
+        FROM (numbers)
+        LAST;
+    
+    EXPECT_EQ(result2, 3);
 }
